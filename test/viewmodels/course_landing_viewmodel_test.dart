51b2bfe3fbf2ded0559eac76c592ab9434801afe
Dane Mackier <dane.mackier@outlook.com>
2023-03-29 05:51:53 +0200

    fix: Broke test

--- test/viewmodels/course_landing_viewmodel_test.dart
+++ test/viewmodels/course_landing_viewmodel_test.dart
@@ -1,7 +1,7 @@
+import 'package:filledstacked_academy/app/app.locator.dart';
 import 'package:filledstacked_academy/models/models.dart';
 import 'package:filledstacked_academy/ui/views/course_landing/course_landing_viewmodel.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:filledstacked_academy/app/app.locator.dart';
 
 import '../helpers/test_helpers.dart';
 
@@ -11,40 +11,44 @@ void main() {
     tearDown(() => locator.reset());
 
     final List<Course> testCourses = [
-      Course(id: 'flutter-web', title: 'Master Flutter on The Web', modules: [
-        Module(
-          id: 'prerequisites',
-          title: 'Prerequisites',
-          chapters: [
-            Chapter(
-              id: 'readme',
-              title: 'README',
-              description:
-                  'Everything you need to know before starting this course.',
-              durationInSeconds: 450,
+      Course(
+          id: 'flutter-web',
+          readableId: 'flutter-web',
+          title: 'Master Flutter on The Web',
+          modules: [
+            Module(
+              id: 'prerequisites',
+              title: 'Prerequisites',
+              chapters: [
+                Chapter(
+                  id: 'readme',
+                  title: 'README',
+                  description:
+                      'Everything you need to know before starting this course.',
+                  durationInSeconds: 450,
+                ),
+                Chapter(
+                  id: 'basics',
+                  title: 'Stacked Basics',
+                  description:
+                      'Get to know Stacked, what it provides, how to use the CLI and how it works',
+                  durationInSeconds: 683,
+                ),
+              ],
             ),
-            Chapter(
-              id: 'basics',
-              title: 'Stacked Basics',
-              description:
-                  'Get to know Stacked, what it provides, how to use the CLI and how it works',
-              durationInSeconds: 683,
+            Module(
+              id: 'app',
+              title: 'App',
+              chapters: [
+                Chapter(
+                  id: 'what-are-we-building',
+                  title: 'What we will build',
+                  description: 'Everything you need to know to build',
+                  durationInSeconds: 3578,
+                ),
+              ],
             ),
-          ],
-        ),
-        Module(
-          id: 'app',
-          title: 'App',
-          chapters: [
-            Chapter(
-              id: 'what-are-we-building',
-              title: 'What we will build',
-              description: 'Everything you need to know to build',
-              durationInSeconds: 3578,
-            ),
-          ],
-        ),
-      ])
+          ])
     ];
 
     group('extractAllChapters -', () {
