ead349deb7245a06a37d4a8482b47c6a24836448
Fernando Ferrara <ferrarafer@gmail.com>
2023-03-31 11:53:44 -0300

    Add inputFormatters to the Form

--- lib/ui/views/payment_capture/payment_capture_view.desktop.dart
+++ lib/ui/views/payment_capture/payment_capture_view.desktop.dart
@@ -4,7 +4,9 @@ import 'package:filledstacked_academy/ui/common/ui_helpers.dart';
 import 'package:filledstacked_academy/ui/views/payment_capture/payment_capture_view.form.dart';
 import 'package:filledstacked_academy/ui/widgets/common/academy_button.dart';
 import 'package:filledstacked_academy/ui/widgets/common/input_field.dart';
+import 'package:filledstacked_academy/utils/card_helper.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:stacked/stacked.dart';
 
 import 'payment_capture_viewmodel.dart';
@@ -49,23 +51,42 @@ class PaymentCaptureViewDesktop
             InputField(
               controller: cardNumberController,
               hintText: 'Card Number',
+              inputFormatters: [
+                FilteringTextInputFormatter.digitsOnly,
+                LengthLimitingTextInputFormatter(16),
+                CardNumberInputFormatter(),
+              ],
+              textInputType: TextInputType.number,
             ),
             verticalSpaceSmall,
             InputField(
               controller: cardOwnerController,
-              hintText: 'Full Name',
+              hintText: 'Name on Card',
+              inputFormatters: [nameOnCardInputFormatter],
+              textInputType: TextInputType.name,
             ),
             verticalSpaceSmall,
             InputField(
               controller: cardExpiryController,
               errorText: viewModel.cardExpiryValidationMessage,
               hintText: 'MM / YY',
+              inputFormatters: [
+                FilteringTextInputFormatter.digitsOnly,
+                LengthLimitingTextInputFormatter(4),
+                CardMonthInputFormatter(),
+              ],
+              textInputType: TextInputType.number,
             ),
             verticalSpaceSmall,
             InputField(
               controller: cardCvvController,
               errorText: viewModel.cardCvvValidationMessage,
               hintText: 'CVV',
+              inputFormatters: [
+                FilteringTextInputFormatter.digitsOnly,
+                LengthLimitingTextInputFormatter(3),
+              ],
+              textInputType: TextInputType.number,
             ),
             verticalSpaceMedium,
             Row(
