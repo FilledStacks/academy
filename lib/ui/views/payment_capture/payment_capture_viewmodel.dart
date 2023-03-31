d4e3a89a884987879e597081f4537d0a3f26be69
Fernando Ferrara <ferrarafer@gmail.com>
2023-03-30 22:45:55 -0300

    Set and Read fields from PaymentCaptureViewModel
    - Set cardOwner field with fullName of currentUser
    - Read cardExpiry and cardCvv to show a custom validation message

--- lib/ui/views/payment_capture/payment_capture_viewmodel.dart
+++ lib/ui/views/payment_capture/payment_capture_viewmodel.dart
@@ -1,6 +1,7 @@
 import 'package:filledstacked_academy/app/app.locator.dart';
 import 'package:filledstacked_academy/app/app.logger.dart';
 import 'package:filledstacked_academy/app/app.router.dart';
+import 'package:filledstacked_academy/services/user_service.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
@@ -9,12 +10,17 @@ import 'payment_capture_view.form.dart';
 class PaymentCaptureViewModel extends FormViewModel {
   final log = getLogger('PaymentCaptureViewModel');
   final _routerService = locator<RouterService>();
+  final _userService = locator<UserService>();
+
+  Future<void> initializeForm() async {
+    cardOwnerValue = _userService.currentUser.fullName;
+  }
 
   Future<void> onAccept() async {
     log.i('Payment submited!');
 
     if (!isFormValid) {
-      setValidationMessage('Something went wrong, please verify data entered.');
+      setValidationMessage(_composeValidationMessageText());
       rebuildUi();
       return;
     }
@@ -26,5 +32,22 @@ class PaymentCaptureViewModel extends FormViewModel {
     log.i('Payment cancelled!');
     clearForm();
     await _routerService.clearStackAndShow(HomeViewRoute());
+  }
+
+  String _composeValidationMessageText() {
+    String output =
+        'Something went wrong, please verify the following errors:\n';
+
+    if (hasCardExpiryValidationMessage) {
+      output +=
+          '\n- EXPIRY value: "$cardExpiryValue". Error: $cardExpiryValidationMessage.';
+    }
+
+    if (hasCardCvvValidationMessage) {
+      output +=
+          '\n- CVV value: "$cardCvvValue". Error: $cardCvvValidationMessage.';
+    }
+
+    return output;
   }
 }
