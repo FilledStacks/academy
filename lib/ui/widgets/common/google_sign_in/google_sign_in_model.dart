1c584cdd1e5258bcb0eced7191f8a4c528b6e4f1
Fernando Ferrara <ferrarafer@gmail.com>
2023-04-14 01:57:59 -0300

    Add GoogleSignIn common widget
    Add a GoogleSignIn button widget which can be used anywhere and there is
    no need to add sign in logic to any other viewmodel.

---
+++ lib/ui/widgets/common/google_sign_in/google_sign_in_model.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+
+import 'package:filledstacked_academy/app/app.locator.dart';
+import 'package:filledstacked_academy/app/app.logger.dart';
+import 'package:filledstacked_academy/enums/bottom_sheet_type.dart';
+import 'package:filledstacked_academy/enums/sign_in_result.dart';
+import 'package:filledstacked_academy/services/analytics_service.dart';
+import 'package:filledstacked_academy/services/user_service.dart';
+import 'package:filledstacked_academy/ui/common/app_strings.dart';
+import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+class GoogleSignInModel extends BaseViewModel {
+  final log = getLogger('GoogleSignInModel');
+  final _analyticsService = locator<AnalyticsService>();
+  final _sheetService = locator<BottomSheetService>();
+  final _userService = locator<UserService>();
+
+  Future<void> signInWithGoogle() async {
+    log.i('');
+
+    unawaited(_analyticsService.logButtonClick(name: ksCTASignInWithGoogle));
+
+    final result = await runBusyFuture(_userService.signInWithGoogle());
+
+    if (result == SignInResult.failure) {
+      await _sheetService.showCustomSheet(
+        variant: BottomSheetType.notice,
+        title: 'Sign in failed',
+        description: 'Please, try again later or contact support',
+      );
+
+      return;
+    }
+  }
+}
