ff8007f80004933295d80aee1e596c60929df38c
Dane Mackier <dane.mackier@outlook.com>
2023-04-20 12:45:54 +0200

    Adds input formatters

---
+++ lib/utils/input_formatters.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/services.dart';
+
+/// TextInputFormatter to automatically add double spaces
+/// after every fourth character inputted in a TextFormField.
+class CardNumberInputFormatter extends TextInputFormatter {
+  @override
+  TextEditingValue formatEditUpdate(
+      TextEditingValue oldValue, TextEditingValue newValue) {
+    var text = newValue.text;
+    if (newValue.selection.baseOffset == 0) {
+      return newValue;
+    }
+    var buffer = StringBuffer();
+    for (int i = 0; i < text.length; i++) {
+      buffer.write(text[i]);
+      var nonZeroIndex = i + 1;
+      if (nonZeroIndex % 4 == 0 && nonZeroIndex != text.length) {
+        buffer.write('  '); // Add double spaces.
+      }
+    }
+    var string = buffer.toString();
+    return newValue.copyWith(
+        text: string,
+        selection: TextSelection.collapsed(offset: string.length));
+  }
+}
+
+/// TextInputFormatter to allow only characters for name on the card
+TextInputFormatter get nameOnCardInputFormatter {
+  return FilteringTextInputFormatter.allow(RegExp('[a-zA-Z ]'));
+}
+
+/// TextInputFormatter to automatically add a slash
+/// after every second character inputted in a TextFormField.
+class CardMonthInputFormatter extends TextInputFormatter {
+  @override
+  TextEditingValue formatEditUpdate(
+    TextEditingValue oldValue,
+    TextEditingValue newValue,
+  ) {
+    var newText = newValue.text;
+    if (newValue.selection.baseOffset == 0) {
+      return newValue;
+    }
+    var buffer = StringBuffer();
+    for (int i = 0; i < newText.length; i++) {
+      buffer.write(newText[i]);
+      var nonZeroIndex = i + 1;
+      if (nonZeroIndex % 2 == 0 && nonZeroIndex != newText.length) {
+        buffer.write(' / ');
+      }
+    }
+    var string = buffer.toString();
+    return newValue.copyWith(
+        text: string,
+        selection: TextSelection.collapsed(offset: string.length));
+  }
+}
